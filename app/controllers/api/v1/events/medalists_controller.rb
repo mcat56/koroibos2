47e5d4170b89706d194027ec0cb463d3004e8a3f
Mary Lang <mcl86@cornell.edu>
2020-03-02 15:29:50 -0700

    Add tests and application controller rescue

--- app/controllers/api/v1/events/medalists_controller.rb
+++ app/controllers/api/v1/events/medalists_controller.rb
@@ -2,15 +2,11 @@ class Api::V1::Events::MedalistsController < ApplicationController
 
   def index
     event = Event.find(params[:id])
-    if !event
-      render status: :not_found
-    else
-      medalists = event.get_medalists
-      result = { event: event.name,
-        medalists: [] }
-      medalists.each {|medalist| result[:medalists] << MedalistSerializer.new(event, medalist)}
-      result[:medalists]
-      render status: :ok, json: result
-    end
+    medalists = event.get_medalists
+    result = { event: event.name,
+      medalists: [] }
+    medalists.each {|medalist| result[:medalists] << MedalistSerializer.new(event, medalist)}
+    result[:medalists]
+    render status: :ok, json: result
   end
 end
