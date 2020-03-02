a01f5c90fba33e58d8ca7ad052a6aaf4637442f4
Mary Lang <mcl86@cornell.edu>
2020-03-01 20:55:02 -0700

    Add condition for filtering by age Specs passing

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::OlympiansController < ApplicationController
 
   def index
-    olympians = Olympian.all
+    if params[:age] == 'youngest'
+      olympians = Olympian.order(:age).limit(1)
+    elsif params[:age] == 'oldest'
+      olympians = Olympian.(age: :desc).limit(1)
+    else
+      olympians = Olympian.all
+    end
+
     result = { olympians: [] }
     olympians.each { |olympian| result[:olympians] << OlympianSerializer.new(olympian) }
     render json: result
