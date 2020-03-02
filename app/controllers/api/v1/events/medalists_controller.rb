46be7bfbb821b1138a700ef746f723d8ec02fcc7
Mary Lang <mcl86@cornell.edu>
2020-03-02 14:49:13 -0700

    Add status with condition

--- app/controllers/api/v1/events/medalists_controller.rb
+++ app/controllers/api/v1/events/medalists_controller.rb
@@ -2,11 +2,15 @@ class Api::V1::Events::MedalistsController < ApplicationController
 
   def index
     event = Event.find(params[:id])
-    medalists = event.get_medalists
-    result = { event: event.name,
-      medalists: [] }
-    medalists.each {|medalist| result[:medalists] << MedalistSerializer.new(event, medalist)}
-    result[:medalists]
-    render json: result
+    if !event
+      render status: :not_found
+    else
+      medalists = event.get_medalists
+      result = { event: event.name,
+        medalists: [] }
+      medalists.each {|medalist| result[:medalists] << MedalistSerializer.new(event, medalist)}
+      result[:medalists]
+      render status: :ok, json: result
+    end
   end
 end
