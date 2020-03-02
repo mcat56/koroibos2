c68da6f63f2bdc0f13cdca704b540dc7aa757f9c
Mary Lang <mcl86@cornell.edu>
2020-03-01 20:09:36 -0700

    Add total medals to olympian model

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -6,4 +6,9 @@ class Olympian < ApplicationRecord
   has_many :olympian_events
   has_many :events, through: :olympian_events
   has_many :games, through: :events
+
+
+  def total_medals
+    OlympianEvent.where(olympian_id: self.id, medal: ['Bronze', 'Silver', 'Gold']).length
+  end
 end
